44cde693778bb2f4f16d2a53a31d578146fd6171
Patel Bhavin <techie.bhavin@gmail.com>
2026-01-11 16:00:22 +0530

    Add backend models and authentication with JWT

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
+const PORT = process.env.PORT || 5006;
+
 app.use(cors());
 app.use(express.json());
+
+app.use("/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("API running...");
@@ -16,8 +22,10 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(5000, () => {
-      console.log("Server running on port 5000");
-    });
   })
   .catch((err) => console.log(err));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  // console.log("JWT_SECRET:", process.env.JWT_SECRET);
+});
